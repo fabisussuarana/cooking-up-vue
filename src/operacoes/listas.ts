d8008f4743e60ec87120f0182c0d6fd776b8fb3f
Fabiana Sussuarana <fabianasussuarana@Mac.local>
2025-12-13 17:12:46 -0500

    comentário sobre a função every

--- src/operacoes/listas.ts
+++ src/operacoes/listas.ts
@@ -1,3 +1,4 @@
 export function itensDeLista1EstaoEmLista2(lista1: unknown[], lista2: unknown[]) {
+  // every serve para verificar se todos os elementos de uma lista satisfazem uma condição
   return lista1.every((itemLista1) => lista2.includes(itemLista1));
 }
